01c9a28e7bd8d0ddd65bde190058769595a7a9ea
tylerguy <tylerdenman200@gmail.com>
2024-07-23 09:57:23 +0100

    remove scoped styles

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue'
+import { RouterView } from 'vue-router'
 import BaseHeader from './components/BaseHeader.vue'
 </script>
 
 <template>
-    <NavBar />
   <BaseHeader />
 
   <div id="content" style="display: block">
-  <RouterView />
+    <Suspense>
+      <RouterView> </RouterView>
+    </Suspense>
   </div>
 </template>
-
-<style scoped>
-*
-{
-  box-sizing: border-box;
-}
-
-div {
-  unicode-bidi: isolate;
-}
-
-body {
-  font-family: minecraftia,Zpix,monospace;
-    color: #eee;
-    background-position: left top,left 64px,left 128px;
-    background: linear-gradient(-235deg,#565eb5 10%,#fd90af 100%);
-}
-</style>
